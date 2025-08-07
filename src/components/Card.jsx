f8893f0792d09e030c63836ceb201af339af0d88
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-08-07 11:41:49 +0600

    Card some design add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,18 +7,27 @@ import { FaBagShopping } from 'react-icons/fa6'
 
 const Card = ({title, price, category, src }) => {
   return (
-    <div className='w-[300px] h-[478px] relative group border border-[#0000001a]'>
-        <div className='w-full h-[330px]'><Image className='w-full h-full' src={src}/></div>
-        <p className='text-black text-base font-semibold font-san px-4 pt-8'>{title}</p>
-        <Flex className='px-4 justify-between pt-5'>
+    <div className='w-[300px] h-[448px] relative group border border-[#0000001a]'>
+        <div className='w-full h-[300px] lg:h-[330px]'><Image className='w-full h-full' src={src}/></div>
+        <p className='text-black text-base font-semibold font-san px-4 pt-5 lg:pt-8'>{title}</p>
+        <Flex className='px-4 justify-between pt-3 lg:pt-5'>
             <span className='text-[#00000080] text-base font-normal font-san'>{category}</span>
             <span className='text-black text-base font-semibold font-san '>{price}</span>
         </Flex>
-        <div className='absolute left-0 bottom-0 bg-black w-full h-[55px] flex justify-between items-center px-5 opacity-0 group-hover:opacity-95 group-hover:bottom-[147px] duration-500'>
+        <div className='hidden lg:block'>
+          <div className='absolute left-0 bottom-0 bg-black w-full h-[55px] flex justify-between items-center px-5 opacity-0 group-hover:opacity-95 group-hover:bottom-[116px] duration-500'>
             <FaRegHeart className='text-white'/> 
             <span className='flex gap-x-3 items-center'><FaBagShopping className='text-white'/> <span className='text-white text-sm font-semibold font-san'>Shop Now</span></span>
         </div>
+        </div>
         <span className='bg-black py-1 px-2 text-sm text-white font-semibold font-san absolute top-5 left-0'>Sale</span>
+
+        <div className='lg:hidden'>
+          <div className='absolute left-0 bottom-0 bg-black w-full h-[55px] flex justify-between items-center px-5 opacity-100 duration-500'>
+            <FaRegHeart className='text-white'/> 
+            <span className='flex gap-x-3 items-center'><FaBagShopping className='text-white'/> <span className='text-white text-sm font-semibold font-san'>Shop Now</span></span>
+        </div>
+        </div>
     </div>
   )
 }
