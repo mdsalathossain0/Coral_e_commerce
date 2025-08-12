8f6eac2a870c7b42aed0680b5274a1e5db3b6024
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-08-12 21:04:22 +0600

    bread problem solved

--- src/pages/ArtCollectibles.jsx
+++ src/pages/ArtCollectibles.jsx
@@ -11,12 +11,14 @@ const ArtCollectibles = () => {
   let prev = useSelector((state)=>state.bread.prevvalue)
   let current = useSelector((state)=>state.bread.currentvalue)
 
+  console.log('Prev:', JSON.stringify(prev))
+
   return (
     <section className='py-10 lg:py-15'>
       <Container>
         <Heading text='Art & Collectibles' />
 
-        <p className='pb-10'>{prev && current!==prev && prev === 'artcollectibles' ? `${prev} >` : null  } artcollectibles</p>
+        <p className='pb-10'>{prev && prev!==current ? `${prev} >` : ''  } Art & Collectibles</p>
       </Container>
       <Image src={Message} />
     </section>
